9704fc617ddd8cfcf1f13a7d0869b3b5d5e36e83
Alvaro Garcia <alvarogarcia7@users.noreply.github.com>
2026-06-04 22:17:50 +0400

    test node with all attributes

--- python/test_radix.py
+++ python/test_radix.py
@@ -10,3 +10,8 @@ class TestRadix:
         assert node.children == []
         assert node.is_final
 
+    def test_node_with_all_arguments(self):
+        node = Node("a", True, ['a'])
+        assert node.word == "a"
+        assert node.children == ['a']
+        assert node.is_final
